d21328f7e45c8331c0e3a63327e87d2e8babecc3
Abubakar <abubakaryung@gmail.com>
2023-03-16 10:44:07 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * main - a programm with the outcome piece of art using printf 
+ * main - print " a programm with the outcome piece of art using printf" 
  * Return: 0
+ *
  */
 
 int main(void)
+
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 }
