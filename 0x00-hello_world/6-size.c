14ca54622eadcdbd032d15a40046f1836be3eae4
Abubakar <abubakaryung@gmail.com>
2023-03-16 18:34:53 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints ("the size of various types on the computer it is compiled and run on.")
+ * Return: 0 
+ */
+int main(void)
+{
+	char v;
+	int w;
+	long int x;
+	long long int y;
+	float f;
+
+printf("size of a char: %zu byte(s)\n", sizeof(char));
+printf("size of an int: %zu bytes(s)\n",sizeof(int));
+printf("size of an long int: %zu bytes(s)\n",sizeof(long int));
+printf("size of an long long int: %zu bytes(s)\n",sizeof(long long int));
+printf("size of an float: %zu bytes(s)\n",sizeof(float));
+return (0);
+}
