3b439307f9bbd1669c6874064ddc5a09b280d019
Abubakar <abubakaryung@gmail.com>
2023-03-20 13:01:44 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+	char d;
+
+	c = 'a';
+	h = 'A';
+	while (c <= 'z')
+	{
+		putchar (c);
+		c++;
+	}
+	while (h <= 'z')
+	{
+		putchar(h);
+		h++;
+	}
+	putchar('\n');
+	return (0);
