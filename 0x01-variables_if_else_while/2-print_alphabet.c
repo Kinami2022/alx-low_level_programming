9fc5ea6f3ad67f894f35828a6b186f475f6065c9
Abubakar <abubakaryung@gmail.com>
2023-03-20 03:22:53 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 /**
- * main -Entry point 
+ * main - Prints the alphabet
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	char c;
-	int a;
-	c = 'a';
-	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
-		}
-	putchar('\n');
-	return (0);
+char alp[26] - "abcdefghijklmnopqrstuvwxyz";
+int n;
+for (n = 0; n < 26; n++)
+{
+putchar(alp[1]);
+}
+putchar('\n');
+return (0);
 }
