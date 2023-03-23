a26618c77bfd1bb29775a510b59766cda9c35c89
Abubakar <abubakaryung@gmail.com>
2023-03-22 21:35:20 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,8 +2,8 @@
 
 /**
  * _isalpha - checks for alphabetic character
- * @c: the charcter to be checked 
- * Return: 1 for alphabetic character or 0 for anything else 
+ * @c: the charcter to be checked
+ * Return: 1 for alphabetic character or 0 for anything else
  */
 
 int _isalpha(int c)
