0693fe575a1abbdc36f22ac7b1b13e83f9ccfbff
Abubakar <abubakaryung@gmail.com>
2023-03-23 03:02:57 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * Sum - sum two value
+ * @n1: the first number to be computed
+ * @n2: the second number to be  computed
+ * Return: sum of integer
+ */
+
+int Sum(int n1, int n2)
+{
+	return (n1 + n2);
+}
