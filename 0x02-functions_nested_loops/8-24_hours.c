73d79d157fb6730901ebe8fd5d1628efbac7cb0b
Abubakar <abubakaryung@gmail.com>
2023-03-23 02:38:18 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,16 +8,16 @@
 void jack_bauer(void)
 
 {
-	int w, x, y, z;
-	for (w = 0; w <= 2; w++)
+int w, x, y, z;
+for (w = 0; w <= 2; w++)
 	{
-		for (x = 0; x <= 9; x++ )
+for (x = 0; x <= 9; x++ )
 		{
 			if ((w <= 1 && x <= 9) || (w <= 2 && x <= 3))
 			{
-				for (y = 0; y <= 5; y++)
+for (y = 0; y <= 5; y++)
 				{
-					for (z = 0; z <= 9; z++)
+for (z = 0; z <= 9; z++)
 					{
 						_putchar(w + '0');
 						_putchar(x + '0');
